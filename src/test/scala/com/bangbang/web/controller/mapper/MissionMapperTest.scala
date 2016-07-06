1dcadcb308dfe5482350e55593a1c84d2df48ce3
Zhranklin <chigou79@outlook.com>
2016-07-06 21:48:51 +0800

    archive

---
+++ src/test/scala/com/bangbang/web/controller/mapper/MissionMapperTest.scala
@@ -0,0 +1,8 @@
+package com.bangbang.web.controller.mapper
+
+/**
+  * Created by Zhranklin on 16/7/6.
+  */
+class MissionMapperTest {
+
+}
--- src/test/scala/com/bangbang/web/controller/mapper/testtest.scala
+++
@@ -1,8 +0,0 @@
-package com.bangbang.web.controller.mapper
-
-/**
-  * Created by Zhranklin on 16/7/5.
-  */
-class testtest {
-
-}
